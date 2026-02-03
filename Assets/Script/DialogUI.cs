9ffb9acdab2b413886a2f1e6e69c59745be9154a
KimKyeongTae-meta <axzy123654@gmail.com>
2026-02-03 16:54:52 +0900

    맵이동, 다가가면 !, 대화창 등등

---
+++ Assets/Script/DialogUI.cs
@@ -0,0 +1,41 @@
+﻿using TMPro;
+using UnityEngine;
+using static UnityEditor.Rendering.MaterialUpgrader;
+
+public class DialogUI : MonoBehaviour
+{
+
+    public GameObject panel;
+    public TMP_Text dialogText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        panel.SetActive(false);
+        
+    }
+
+    public void Conversation(string line)
+    {
+        panel.SetActive(true);
+        dialogText.text = line;
+    }
+
+    public void ShowConversationList(string[] titles)
+    {
+        Debug.Log("ShowConversationList CALLED");
+
+        panel.SetActive(true);
+
+    }
+
+    public void Hide()
+    {
+               panel.SetActive(false);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
