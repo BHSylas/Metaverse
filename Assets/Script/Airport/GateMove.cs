631d1a7dcae4c0c8167c7c62b61f2fb944ea20a2
KimKyeongTae-meta <axzy123654@gmail.com>
2026-02-09 15:46:03 +0900

    서버에서 정보 받아와 해당 맵으로 이동

---
+++ Assets/Script/Airport/GateMove.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+public class GateMove : MonoBehaviour
+{
+
+    public string TargetScene;
+
+    public void SetTargetScene(string sceneName)
+    {
+        TargetScene = sceneName;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!collision.CompareTag("Player")) 
+            return;
+
+        if (string.IsNullOrEmpty(TargetScene))
+        {
+            Debug.LogWarning("TargetScene X");
+            return;
+        }
+
+        Debug.Log("이동: " + TargetScene);
+
+        PlayerPrefs.SetString("NextScene", TargetScene);
+
+        PlayerPrefs.Save();
+
+
+        StartCoroutine(LoadWithDelay());
+
+        
+
+    }
+
+
+    private IEnumerator LoadWithDelay()
+    {
+        yield return null; // ⭐ 한 프레임 대기
+        SceneManager.LoadScene("LoadingScene");
+    }
+
+
+
+
+
+}
